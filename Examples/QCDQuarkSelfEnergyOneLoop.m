42da0d576d4d15c57c9a0500b18da1f738faa5b7
Vladyslav Shtabovenko <v.shtabovenko@tum.de>
2016-02-04 10:41:28 +0100

    Added QCD quark 1-loop self-energy example with loop integrals evaluated using Package X via FeynHelpers.

---
+++ Examples/QCDQuarkSelfEnergyOneLoop.m
@@ -0,0 +1,82 @@
+(* ::Package:: *)
+
+(* :Title: QCDQuarkSelfEnergyOneLoop                                        *)
+
+(*
+	This software is covered by the GNU Lesser General Public License 3.
+	Copyright (C) 1990-2015 Rolf Mertig
+	Copyright (C) 1997-2015 Frederik Orellana
+	Copyright (C) 2014-2015 Vladyslav Shtabovenko
+*)
+
+(* :Summary:  Computation of the quark self-energy in QCD at 1-loop         *)
+
+(* ------------------------------------------------------------------------ *)
+
+
+(* ::Section:: *)
+(*The quark self-energy in QCD at  1-loop *)
+
+
+(* ::Subsection:: *)
+(*Load FeynCalc, FeynArts and FeynHelpers*)
+
+
+If[ $FrontEnd === Null,
+		$FeynCalcStartupMessages = False;
+		Print["Computation of the quark self-energy in QCD at 1-loop"];
+];
+$LoadAddOns={"FeynHelpers"};
+$LoadFeynArts = True;
+<<FeynCalc`
+$FAVerbose=0;
+
+
+(* ::Subsection:: *)
+(*Generate Feynman diagrams*)
+
+
+Paint[diags = InsertFields[CreateTopologies[1, 1 -> 1,
+		ExcludeTopologies -> {Tadpoles}], {F[3,{1}]} -> {F[3,{1}]},
+		InsertionLevel -> {Classes}, GenericModel -> "Lorentz",
+		Model -> "SMQCD",ExcludeParticles->{S[1],S[2],S[3],V[1],V[2],V[3]}], ColumnsXRows -> {1, 1},
+		SheetHeader -> False,SheetHeader->None,Numbering -> None,ImageSize->{256,256}];
+
+
+amps=FCFAConvert[CreateFeynAmp[diags, Truncated -> True,GaugeRules->{},PreFactor->-1],IncomingMomenta->{p},
+OutgoingMomenta->{p},LoopMomenta->{q},DropSumOver->True,UndoChiralSplittings->True,ChangeDimension->D,List->False]/.{MU->M,GaugeXi[g]->GaugeXi}
+
+
+ampsEval=amps//Contract//SUNSimplify//TID[#,q]&
+
+
+res1=PaXEvaluate[ampsEval,q,PaXImplicitPrefactor->1/(2Pi)^(4-2 Epsilon)]
+
+
+(* ::Text:: *)
+(*This is I*Sigma (divergent part only)*)
+
+
+quarkSelfEnergy=-SelectNotFree[res1,Epsilon]//Expand
+
+
+(* ::Text:: *)
+(*We can compare this result to Eq. 2.5.138 in Foundations of QCD by T. Muta.*)
+
+
+quarSelfEnergyMuta=I*(-Gstrong^2/(4Pi)^2 CF*(3+GaugeXi)(1/Epsilon)*M+GS[p]*Gstrong^2/(4Pi)^2*
+		CF*GaugeXi*(1/Epsilon))SDF[Col1,Col2]//FCI;
+Print["Check with Muta, Eq 2.5.138: ",
+			If[Simplify[quarkSelfEnergy-FCI[quarSelfEnergyMuta]]===0, "CORRECT.", "!!! WRONG !!!"]];
+
+
+(* ::Text:: *)
+(*Another cross-check is to compare to Eq. 16.76 in Peskin and Schroeder, where the authors compute the self-energy diagram for massless quarks in Feynman gauge. All we need to do is just set the quarks mass M to zero and the gauge parameter GaugeXi to 1.*)
+
+
+quarkSelfEnergyMassless=quarkSelfEnergy/.{M->0,GaugeXi->1}
+
+
+ampsSingMasslessPeskin=I*Gstrong^2/(4Pi)^2*GS[p]*CF*(1/Epsilon)SDF[Col1,Col2]//FCI;
+Print["Check with Peskin and Schroeder, Eq 16.76: ",
+			If[Simplify[quarkSelfEnergyMassless-FCI[ampsSingMasslessPeskin]]===0, "CORRECT.", "!!! WRONG !!!"]];
---
+++ Tests/examples.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This small bash script provides a nice way to check that
+# FeynHelpers is working properly using real-life examples.
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
+if [ -z ${MATH+x} ]; then MATH=math; else echo $MATH; fi
+
+
+#QCD Examples
+pwd
+$MATH -nopromt -script ../Examples/QCDQuarkSelfEnergyOneLoop.m
