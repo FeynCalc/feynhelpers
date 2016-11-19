71761dd6035a2f9cf98f9a9317c61f95a0c2a144
Vladyslav Shtabovenko <dev@shtabovenko.df-kunde.de>
2016-11-19 20:08:08 +0100

    Added examples that will appear in the publication.

---
+++ Examples/QCD/NRQCDVertexMatchingInTheTwoQuarkSectorOneLoop.m
@@ -0,0 +1,221 @@
+(* ::Package:: *)
+
+(* :Title: NRQCDVertexMatchingInTheTwoQuarkSectorOneLoop                                        *)
+
+(*
+	This software is covered by the GNU General Public License 3.
+	Copyright (C) 1990-2016 Rolf Mertig
+	Copyright (C) 1997-2016 Frederik Orellana
+	Copyright (C) 2014-2016 Vladyslav Shtabovenko
+*)
+
+(* :Summary:  Computation of the QCD side of the QCD/NRQCD matching in the
+			   two quark sector by expanding quark-gluon vertex in the relative
+			   momentum                                                     *)
+
+(* ------------------------------------------------------------------------ *)
+
+
+(* ::Section:: *)
+(*QCD side of the matching to NRQCD (quark-gluon vertex only) at  1-loop *)
+
+
+(* ::Text:: *)
+(*This example uses a custom QCD in background field formalism model created with FeynRules. Please evaluate the file FeynCalc/Examples/FeynRules/QCDBGF/GenerateModelQCDBGF.m before running it for the first time*)
+
+
+(* ::Subsection:: *)
+(*Load FeynCalc, FeynArts and FeynHelpers*)
+
+
+If[ $FrontEnd === Null,
+		$FeynCalcStartupMessages = False;
+		Print["Computation of the QCD side of the QCD/NRQCD matching in the 
+two quark sector by expanding quark-gluon vertex in the relative momentum"];
+];
+$LoadAddOns={"FeynHelpers"};
+$LoadFeynArts = True;
+<<FeynCalc`
+$FAVerbose=0;
+
+
+(* ::Subsection:: *)
+(*Generate Feynman diagrams*)
+
+
+topVertex = CreateTopologies[1,2 -> 1,ExcludeTopologies -> {WFCorrections}];
+diagsVertex = InsertFields[topVertex, {F[3, {1}],V[50]} ->
+		{F[3, {1}]}, InsertionLevel -> {Classes},		
+ Model -> FileNameJoin[{"QCDBGF","QCDBGF"}],GenericModel->FileNameJoin[{"QCDBGF","QCDBGF"}]];
+Paint[diagsVertex, ColumnsXRows -> {2, 1},ImageSize->{512,256}, Numbering -> None,SheetHeader->False];
+
+
+(* ::Subsection:: *)
+(*On-shell kinematics*)
+
+
+FCClearScalarProducts[];
+ScalarProduct[p1,p1]=m^2;
+ScalarProduct[p2,p2]=m^2;
+ScalarProduct[q,q]=q2;
+ScalarProduct[p1,p2]=-1/2FCI@ SPD[q,q]+m^2;
+
+
+(* ::Subsection:: *)
+(*Evaluation*)
+
+
+(* ::Text:: *)
+(*Since we are going to evaluate both diagrams one after another, it is convenient to define helper functions that will do the job.*)
+
+
+(* ::Text:: *)
+(*This function obtains the amplitude for further evaluation with FeynCalc*)
+
+
+ampFunc0[diag_]:=FCFAConvert[FCPrepareFAAmp[CreateFeynAmp[diag, 
+Truncated -> False,PreFactor->-1]],IncomingMomenta->{p1,q},
+OutgoingMomenta->{p2},LoopMomenta->{l},UndoChiralSplittings->True,
+DropSumOver->True,List->False,FinalSubstitutions->{SMP["m_u"]->m,
+q->p2-p1,Pair[Momentum[Polarization[___],___],___]:>1},
+ChangeDimension->D,SMP->True]//Contract//SUNSimplify//
+ReplaceAll[#,SUNTF[{x__},__]:>SUNT[x]]&//SUNSimplify[#,Explicit->True]&//
+ReplaceAll[#,SMP["g_s"]^3->4Pi SMP["alpha_s"]SMP["g_s"]]&
+
+
+(* ::Text:: *)
+(*Tensor reduction and simplification of the Dirac algebra*)
+
+
+ampFunc1[expr_]:=expr//TID[#,l,UsePaVeBasis->True,ToPaVe->True]&//DiracSimplify//
+Collect2[#,Spinor,Factoring->Simplify]&;
+
+
+(* ::Text:: *)
+(*Gordon decomposition*)
+
+
+ampFunc2[expr_]:=(expr/.{FCI[(FVD[p1,i_]+
+FVD[p2,i_])SpinorUBarD[p2,m_].SpinorUBarD[p1,m_]]:>
+FCI[SpinorUBarD[p2,m].(2m GAD[i]-I DiracSigma[GAD[i],
+GSD[p2-p1]]).SpinorUD[p1,m]]})//DotSimplify//Collect2[#,LorentzIndex]&;
+
+
+(* ::Text:: *)
+(*Expansion in q^2 up to first order*)
+
+
+ampFunc3[expr_]:=PaXEvaluateUVIRSplit[expr,PaXSeries->{{q2,0,1}},PaXAnalytic->True,
+PaXImplicitPrefactor->(2Pi)^(-D)]//FCHideEpsilon//Collect2[#,LorentzIndex]&;
+
+
+(* ::Text:: *)
+(*Extracts F_1(q^2)*)
+
+
+vectorPart[expr_,fo_:1]:=((expr//SelectFree2[#,DiracSigma]&)/
+FCI[I SMP["g_s"]SUNT[Glu2]SpinorUBarD[p2,m].GAD[Lor1].SpinorUD[p1,m]])//
+Collect2[#,{SMP["Delta_UV"],SMP["Delta_IR"],q2},Factoring->FullSimplify,FCFactorOut->fo]&;
+
+
+(* ::Text:: *)
+(*Extracts F_2(q^2)*)
+
+
+scalarPart[expr_,fo_:1]:=((expr//SelectNotFree2[#,DiracSigma]&)/
+FCI[I SMP["g_s"]SUNT[Glu2](I/(2m))FCI[SpinorUBarD[p2,m]. DiracSigma[GAD[Lor1],
+GSD[p2-p1]].SpinorUD[p1,m]]])//Collect2[#,{SMP["Delta_UV"],SMP["Delta_IR"],q2},
+Factoring->FullSimplify,FCFactorOut->fo]&;
+
+
+(* ::Subsection:: *)
+(*QCD abelian vertex*)
+
+
+abelianVertex=ampFunc0[DiagramExtract[diagsVertex,1]]
+
+
+abelianVertex1=ampFunc1[abelianVertex]
+
+
+abelianVertex2=ampFunc2[abelianVertex1]
+
+
+abelianVertex3=ampFunc3[abelianVertex2]
+
+
+(* ::Text:: *)
+(*This is  F_1^(V)*)
+
+
+resF1V=vectorPart[abelianVertex3,SMP["alpha_s"](CF-CA/2)1/Pi]
+
+
+(* ::Text:: *)
+(*This is  F_2^(V)*)
+
+
+resF2V=scalarPart[abelianVertex3,SMP["alpha_s"](CF-CA/2)1/Pi]
+
+
+(* ::Subsection:: *)
+(*QCD non-abelian vertex*)
+
+
+nonAbelianVertex=ampFunc0[DiagramExtract[diagsVertex,2]];
+
+
+nonAbelianVertex1=ampFunc1[nonAbelianVertex];
+
+
+nonAbelianVertex2=ampFunc2[nonAbelianVertex1]
+
+
+nonAbelianVertex3=ampFunc3[nonAbelianVertex2];
+
+
+(* ::Text:: *)
+(*This is  F_1^(g)*)
+
+
+resF1G=vectorPart[nonAbelianVertex3,SMP["alpha_s"]CA/(8Pi)]
+
+
+(* ::Text:: *)
+(*This is  F_2^(g)*)
+
+
+resF2G=scalarPart[nonAbelianVertex3,SMP["alpha_s"]CA/(8Pi)]
+
+
+(* ::Subsection:: *)
+(*Check with the literature*)
+
+
+(* ::Text:: *)
+(*Here we collect the results for F_1^(V) and F_2^(V) (abelian on-shell quark-gluon vertex) as well as  F_1^(g) and F_2^(g) (non-abelian on-shell quark-gluon vertex)  from Manohar (arXiv:hep-ph/9701294). The explicit values for these form-factors are given in Eqs. 29-30 and Eqs. 33-34 of the above-mentioned preprint.*)
+
+
+F1V=((SMP["alpha_s"]/Pi)(CF-1/2 CA)(1/(2EpsilonUV)+1/EpsilonIR+1-3/2Log[m/ScaleMu]+
+q2/m^2(-1/(3EpsilonIR)-1/8+1/3Log[m/ScaleMu])))
+
+
+F2V=(SMP["alpha_s"]/Pi)(CF-1/2 CA)(1/2+q2/(12m^2))
+
+
+F1G=(SMP["alpha_s"]/(8Pi))CA(2/EpsilonUV+4/EpsilonIR+4-6Log[m/ScaleMu]+
+q2/m^2(-3/EpsilonIR-1+3Log[m/ScaleMu]))
+
+
+F2G=(SMP["alpha_s"]/(8Pi))CA(4/EpsilonIR+6-4Log[m/ScaleMu]+
+q2/m^2(4/EpsilonIR+1-4Log[m/ScaleMu]))
+
+
+diff=Map[Simplify[(PowerExpand[FCShowEpsilon[#[[1]]]/.
+{ScaleMu^2->ScaleMu^2 E^EulerGamma/(4Pi),1/EpsilonIR->2/EpsilonIR,
+1/EpsilonUV->2/EpsilonUV}])-#[[2]],Assumptions->{ScaleMu>0,m>0}]&,{{resF1V,F1V},
+{resF2V,F2V},{resF1G,F1G},{resF2G,F2G}}]
+
+
+Print["Check with Manohar, arXiv:hep-ph/9701294: ",
+			If[diff==={0,0,0,0}, "CORRECT.", "!!! WRONG !!!"]];
---
+++ Examples/QED/MasslessQEDSelfEnergiesTwoLoops.m
@@ -0,0 +1,173 @@
+(* ::Package:: *)
+
+(* :Title: MasslessQEDSelfEnergiesTwoLoops                                       *)
+
+(*
+	This software is covered by the GNU General Public License 3.
+	Copyright (C) 1990-2016 Rolf Mertig
+	Copyright (C) 1997-2016 Frederik Orellana
+	Copyright (C) 2014-2016 Vladyslav Shtabovenko
+*)
+
+(* :Summary:  Computation of the 2-loop self-energies in massless QED  *)
+
+(* ------------------------------------------------------------------------ *)
+
+
+(* ::Section:: *)
+(*2-loop self-energies in massless QED*)
+
+
+(* ::Subsection:: *)
+(*Load FeynCalc, FeynArts and FeynHelpers*)
+
+
+If[ $FrontEnd === Null,
+		$FeynCalcStartupMessages = False;
+		Print["Computation of the 2-loop self-energies in massless QED"];
+];
+$LoadAddOns={"FeynHelpers"};
+$LoadFeynArts= True;
+<< FeynCalc`
+$FAVerbose = 0
+
+
+(* ::Subsection:: *)
+(*Vacuum polarization*)
+
+
+topsPol=CreateTopologies[2, 1 -> 1,ExcludeTopologies -> {Tadpoles}];
+diagsVacuumPol=InsertFields[topsPol, {V[1]} -> {V[1]},
+InsertionLevel -> {Particles},ExcludeParticles->{V[2|3],S[_],U[_],F[1|3|4]}];
+Paint[DiagramExtract[diagsVacuumPol,{1,4,7}],ColumnsXRows->{3,1},
+SheetHeader -> False,   Numbering -> None,ImageSize->{768,256}];
+
+
+(* ::Text:: *)
+(*First of all we need to generate the amplitudes and convert them into FeynCalc notation. We choose l1 and l2 to be the loop momenta and p the external momentum*)
+
+
+ampsVacuumPol=FCFAConvert[CreateFeynAmp[DiagramExtract[diagsVacuumPol,{1,4,7}], 
+Truncated -> True,PreFactor->-1,GaugeRules->{}],
+IncomingMomenta->{p},OutgoingMomenta->{p},LoopMomenta->{l1,l2},DropSumOver->True,
+ChangeDimension->D,UndoChiralSplittings->True,List->False,SMP->True,
+LorentzIndexNames->{\[Mu],\[Nu]}]//ReplaceAll[#,SMP["m_e"]->0]&//Contract//FCTraceFactor
+
+
+(* ::Text:: *)
+(*Calculation of the Dirac trace and tensor reduction*)
+
+
+AbsoluteTiming[ampsVacuumPol1=(ampsVacuumPol/.DiracTrace->Tr)//FCMultiLoopTID[#,{l1,l2}]&]
+
+
+(* ::Text:: *)
+(*Simplification of the loop integrals using shifts in the loop momenta*)
+
+
+ampsVacuumPol2=ampsVacuumPol1//FDS[#,l1,l2]&
+
+
+(* ::Text:: *)
+(*IBP-Reduction using FIRE*)
+
+
+ampsVacuumPol3=FIREBurn[ampsVacuumPol2,{l1,l2},{p}]//FDS[#,l1,l2]&
+
+
+(* ::Text:: *)
+(*Nicer form. Notice that the gauge dependence dropped out in the final result, as it should.*)
+
+
+ampsVacuumPol4=ampsVacuumPol3//Collect2[#,{FeynAmpDenominator},Factoring->Factor2]&
+
+
+(* ::Text:: *)
+(*This is Pi_2 (p^2) up to the 1/(2Pi)^2D prefactor*)
+
+
+resVacuumPol=Cancel[ampsVacuumPol4/(-I FCI[(FVD[p,\[Mu]]FVD[p,\[Nu]]-SPD[p,p]MTD[\[Mu],\[Nu]])])]//FullSimplify
+
+
+(* ::Subsection:: *)
+(*Electron self-energy*)
+
+
+topsSE=CreateTopologies[2, 1 -> 1,ExcludeTopologies -> {Tadpoles}];
+diagsSE=InsertFields[topsSE, {F[2,{1}]} -> {F[2,{1}]},InsertionLevel -> {Particles},
+ExcludeParticles->{V[2|3],S[_],U[_],F[1|3|4]}];
+Paint[DiagramExtract[diagsSE,{1,2,5}],ColumnsXRows->{3,1},SheetHeader -> False,   
+Numbering -> None,ImageSize->{768,256}];
+
+
+(* ::Text:: *)
+(*First of all we need to generate the amplitudes and convert them into FeynCalc notation. We choose l1 and l2 to be the loop momenta and p the external momentum*)
+
+
+ampsSE=FCFAConvert[CreateFeynAmp[DiagramExtract[diagsSE,{1,2,5}], Truncated -> True,
+PreFactor->-1,GaugeRules->{}],IncomingMomenta->{p},OutgoingMomenta->{p},
+LoopMomenta->{l1,l2},DropSumOver->True,ChangeDimension->D,UndoChiralSplittings->True,
+List->False,SMP->True,LorentzIndexNames->{\[Mu]}]//ReplaceAll[#,SMP["m_e"]->0]&//
+Contract//FCTraceFactor
+
+
+(* ::Text:: *)
+(*Calculation of the Dirac trace, tensor reduction and simplification of the Dirac algebra*)
+
+
+AbsoluteTiming[ampsSE1=(ampsSE/.DiracTrace->Tr)//FCMultiLoopTID[#,{l1,l2}]&//DiracSimplify]
+
+
+(* ::Text:: *)
+(*Simplification of the loop integrals using shifts in the loop momenta*)
+
+
+ampsSE2=ampsSE1//FDS[#,l1,l2]&
+
+
+(* ::Text:: *)
+(*IBP-Reduction using FIRE*)
+
+
+ampsSE3=FIREBurn[ampsSE2,{l1,l2},{p}]//FDS[#,l1,l2]&
+
+
+(* ::Text:: *)
+(*Nicer form*)
+
+
+ampsSE4=ampsSE3//Collect2[#,{FeynAmpDenominator},Factoring->FullSimplify]&
+
+
+(* ::Text:: *)
+(*This is Sigma_2V(p^2) up to the 1/(2Pi)^2D prefactor*)
+
+
+resSE=Cancel[ampsSE4/(-I FCI[(GSD[p])])]//FullSimplify
+
+
+(* ::Subsection:: *)
+(*Check with the literature*)
+
+
+(* ::Text:: *)
+(*Check with Eq. 5.18 from Grozin's Lectures on QED and QCD (hep-ph/0508242)*)
+
+
+resGrozinVacuumPol=FCI[SMP["e"]^4 2(D-2)/((D-1)(D-4))(-(D^2-7D+16)FAD[l1,l2,l1-p,l2-p]+
+4(D-3)(D^2-4D+8)/(D-4)(1/SPD[p,p])FAD[l1,l1-l2,l2-p])]//Cancel//FullSimplify
+
+
+(* ::Text:: *)
+(*Check with Eq. 5.51 from Grozin's Lectures on QED and QCD (hep-ph/0508242)*)
+
+
+resGrozinSE=FCI[SMP["e"]^4(D-2)( 2(D-2)/(D-6)(1/SPD[p,p])FAD[l1,l1-l2,l2-p]-
+1/4((D-2)(GaugeXi[A])^2+D-6)FAD[l1,l2,l1-p,l2-p]
++(1/2)(D-3)/(D-4)((3D-8)(GaugeXi[A])^2-D-4)(1/SPD[p,p])FAD[l1,
+l1-l2,l2-p])]//Cancel//FullSimplify
+
+
+Print["Check with the Grozin (hep-ph/0508242): ", If[{Simplify[resGrozinSE-resSE],
+Simplify[resGrozinVacuumPol-resVacuumPol]}==={0,0},
+		"CORRECT.", "!!! WRONG !!!"]];
