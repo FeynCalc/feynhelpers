9b35e5cc93ec31e75e3c330e0ff1aaf1edcadfb1
Vladyslav Shtabovenko <dev@shtabovenko.df-kunde.de>
2016-12-13 20:53:21 +0100

    Updated automatic installer to install Package-X 2.0.3.

--- install.m
+++ install.m
@@ -76,7 +76,7 @@ Options[InstallFeynHelpers]={
 
 Options[InstallPackageX]={
 	AutoOverwritePackageXDirectory -> None,
-	PackageXLink->"http://www.hepforge.org/archive/packagex/X-2.0.2.zip",
+	PackageXLink->"http://www.hepforge.org/archive/packagex/X-2.0.3.zip",
 	InstallPackageXTo->FileNameJoin[{$UserBaseDirectory, "Applications","X"}]
 };
 
