d895947436526676518bce132e6d1a2069d731ad
Vladyslav Shtabovenko <dev.vsht@posteo.de>
2021-03-31 16:57:09 +0200

    Added a mechanism for preventing some of the addons from being loaded.

--- FeynHelpers.m
+++ FeynHelpers.m
@@ -28,17 +28,52 @@ End[]
 
 Begin["`FeynHelpers`Private`"];
 
+If[ !ValueQ[FeynCalc`$FeynHelpersLoadInterfaces],
+	FeynCalc`$FeynHelpersLoadInterfaces = {"PackageX", "FIRE", "Fermat", "QGRAF", "LoopTools"}
+];
+
 $FeynHelpersVersion="1.4.0";
 
 $FeynHelpersDirectory = FileNameJoin[{$FeynCalcDirectory, "AddOns", "FeynHelpers"}];
 
 (* Load the intefaces *)
 BeginPackage["FeynCalc`"];
-FCDeclareHeader/@FileNames[{"*.m"}, FileNameJoin[{$FeynHelpersDirectory,"Interfaces"}]];
-FCDeclareHeader/@FileNames[{"*.m"},ToFileName[{$FeynHelpersDirectory,"Interfaces","QGRAF"}]];
-Get/@FileNames[{"*.m"}, FileNameJoin[{$FeynHelpersDirectory,"Interfaces"}]];
-Get/@FileNames[ToFileName[{$FeynHelpersDirectory,"Interfaces","QGRAF","QGRAFShared.m"}]];
-Get/@FileNames[{"*.m"},ToFileName[{$FeynHelpersDirectory,"Interfaces","QGRAF"}]];
+
+load = "";
+
+If[	!FreeQ[$FeynHelpersLoadInterfaces,"PackageX"],
+	load = FileNameJoin[{$FeynHelpersDirectory,"Interfaces","PackageX.m"}];
+	FCDeclareHeader[load];
+	Get[load]
+];
+
+If[	!FreeQ[$FeynHelpersLoadInterfaces,"FIRE"],
+	load = FileNameJoin[{$FeynHelpersDirectory,"Interfaces","FIRE.m"}];
+	FCDeclareHeader[load];
+	Get[load]
+];
+
+If[	!FreeQ[$FeynHelpersLoadInterfaces,"Fermat"],
+	load = FileNameJoin[{$FeynHelpersDirectory,"Interfaces","Fermat.m"}];
+	FCDeclareHeader[load];
+	Get[load]
+];
+
+If[	!FreeQ[$FeynHelpersLoadInterfaces,"LoopTools"],
+	load = FileNameJoin[{$FeynHelpersDirectory,"Interfaces","LoopTools.m"}];
+	FCDeclareHeader[load];
+	Get[load]
+];
+
+If[	!FreeQ[$FeynHelpersLoadInterfaces,"QGRAF"],
+	load = FileNameJoin[{$FeynHelpersDirectory,"Interfaces","QGRAF","QGRAFShared.m"}];
+	FCDeclareHeader[load];
+	Get[load];
+	load = FileNames[{"*.m"},ToFileName[{$FeynHelpersDirectory,"Interfaces","QGRAF"}]];
+	FCDeclareHeader/@load;
+	Get/@load
+];
+Remove["FeynCalc`load"];
 EndPackage[]
 
 
@@ -70,16 +105,32 @@ If[ $FeynCalcStartupMessages =!= False,
 	Style[" If you use FeynHelpers in your research, please cite","Text"]];
 	Print [Style[" \[Bullet] V. Shtabovenko, \"FeynHelpers: Connecting FeynCalc to FIRE and Package-X\", Comput. Phys. Commun., 218, 48-65, 2017, arXiv:1611.06793","Text"]];
 	Print[Style["Furthermore, remember to cite the authors of the tools that you are calling from FeynHelpers, which are","Text"]];
-	Print [Style[" \[Bullet] "], Style[DisplayForm@ButtonBox["FIRE",ButtonData :> {URL["https://bitbucket.org/feynmanIntegrals/fire/"], None},BaseStyle -> "Hyperlink",
+
+	If[	!FreeQ[$FeynHelpersLoadInterfaces,"FIRE"],
+		Print [Style[" \[Bullet] "], Style[DisplayForm@ButtonBox["FIRE",ButtonData :> {URL["https://bitbucket.org/feynmanIntegrals/fire/"], None},BaseStyle -> "Hyperlink",
 				ButtonNote -> "https://bitbucket.org/feynmanIntegrals/fire/"],"Text"], Style[" by A. Smirnov, if you are using the function FIREBurn.","Text"]];
-	Print [Style[" \[Bullet] "], Style[DisplayForm@ButtonBox["Package-X",ButtonData :> {URL["https://packagex.hepforge.org"], None},BaseStyle -> "Hyperlink",
+	];
+
+	If[	!FreeQ[$FeynHelpersLoadInterfaces,"PackageX"],
+		Print [Style[" \[Bullet] "], Style[DisplayForm@ButtonBox["Package-X",ButtonData :> {URL["https://packagex.hepforge.org"], None},BaseStyle -> "Hyperlink",
 				ButtonNote -> "https://packagex.hepforge.org"],"Text"], Style[" by H. Patel, if you are using the function PaXEvaluate.","Text"]];
-	Print [Style[" \[Bullet] "], Style[DisplayForm@ButtonBox["Fermat",ButtonData :> {URL["https://home.bway.net/lewis"], None},BaseStyle -> "Hyperlink",
+	];
+
+	If[	!FreeQ[$FeynHelpersLoadInterfaces,"Fermat"],
+		Print [Style[" \[Bullet] "], Style[DisplayForm@ButtonBox["Fermat",ButtonData :> {URL["https://home.bway.net/lewis"], None},BaseStyle -> "Hyperlink",
 				ButtonNote -> "https://home.bway.net/lewis"],"Text"], Style[" by R. Lewis, if you are using the function FerSolve.","Text"]];
-	Print [Style[" \[Bullet] "], Style[DisplayForm@ButtonBox["QGRAF",ButtonData :> {URL["http://cfif.ist.utl.pt/~paulo/qgraf.html"], None},BaseStyle -> "Hyperlink",
+	];
+
+	If[	!FreeQ[$FeynHelpersLoadInterfaces,"QGRAF"],
+		Print [Style[" \[Bullet] "], Style[DisplayForm@ButtonBox["QGRAF",ButtonData :> {URL["http://cfif.ist.utl.pt/~paulo/qgraf.html"], None},BaseStyle -> "Hyperlink",
 				ButtonNote -> "http://cfif.ist.utl.pt/~paulo/qgraf.html"],"Text"], Style[" by P. Nogueira, if you are using functions that begin with QG.","Text"]];
-	Print [Style[" \[Bullet] "], Style[DisplayForm@ButtonBox["LoopTools",ButtonData :> {URL["http://www.feynarts.de/looptools/"], None},BaseStyle -> "Hyperlink",
+	];
+
+	If[	!FreeQ[$FeynHelpersLoadInterfaces,"LoopTools"],
+		Print [Style[" \[Bullet] "], Style[DisplayForm@ButtonBox["LoopTools",ButtonData :> {URL["http://www.feynarts.de/looptools/"], None},BaseStyle -> "Hyperlink",
 				ButtonNote -> "http://www.feynarts.de/looptools"],"Text"], Style[" by T. Hahn, if you are using functions that begin with LT.","Text"]];
+	];
+
 ];
 
 If[ !ValueQ[FeynHelpers`Package`paxLoaded],
