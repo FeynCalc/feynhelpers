5073031f622991e661255227a3691f2441bb39e0
Vladyslav Shtabovenko <dev.vsht@posteo.de>
2023-03-03 10:52:44 +0100

    Fixed a unit test in the FIRE test suite.

--- Tests/FIRE/Samples/CreateStartFile-v1.m
+++ Tests/FIRE/Samples/CreateStartFile-v1.m
@@ -1,8 +1,18 @@
 Get["/home/vs/.Mathematica/Applications/FIRE6/FIRE6.m"];
+
+
+If[$FrontEnd===Null,
+  projectDirectory=DirectoryName[$InputFileName],
+  projectDirectory=NotebookDirectory[]
+];
+SetDirectory[projectDirectory];
+Print["Working directory: ", projectDirectory];
+
+
 Internal={p1, p3};
 External={q1};
 Propagators={m3^2 - p1^2, -m1^2 - p1^2 - 2*p1*q1, -p3^2, -m1^2 - p3^2 - 2*p3*q1, -p1^2 + 2*p1*p3 - p3^2};
 Replacements={q1^2 -> m1^2};
 PrepareIBP[];
 Prepare[AutoDetectRestrictions -> True];
-SaveStart["/tmp/prop2LtopoG10/prop2LtopoG10"];
+SaveStart["prop2LtopoG10"];
