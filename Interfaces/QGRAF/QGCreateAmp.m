d60bcefa0aecfdfe444d2df7a0f8c2cc7bb8422c
Vladyslav Shtabovenko <dev.vsht@posteo.de>
2024-03-29 13:33:24 +0100

    Fixed few typos.

--- Interfaces/QGRAF/QGCreateAmp.m
+++ Interfaces/QGRAF/QGCreateAmp.m
@@ -13,7 +13,7 @@
 
 QGCreateAmp::usage=
 "QGCreateAmp[nLoops, {\"InParticle1[p1]\",\"InParticle1[p2]\", ...} ->
-{\"OutParticle1[k1]\", \"OutParticle1[k2]\", ...}]
+{\"OutParticle1[k1]\", \"OutParticle1[k2]\", ...}] 
 calls QGRAF to generate Feynman amplitudes and (optionally) the corresponding
 diagrams, using the specified  model and style files.
 
